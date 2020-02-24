42a79267441bfb3966e017f82d31f543d18feef3
HarshVaragiya <harshvaragiya8@gmail.com>
2020-02-24 22:18:05 +0530

    Added SecureShell Challenge

---
+++ Crypto/SecureShell/script.py
@@ -0,0 +1,22 @@
+from Crypto.PublicKey import ECC
+from Crypto.Protocol.KDF import PBKDF2
+from base64 import b64encode
+from secret import secure_passphrase
+import hashlib
+import random
+
+password = str(random.randint(0,100))
+salt = 'suPer_s3cur3_s@1t'.encode()
+
+def s3cure_rand0m(i):
+    s3cure_rand0m.count +=1
+    rand = PBKDF2((password+hex(s3cure_rand0m.count)).encode(),salt,dkLen=i,count=1)
+    print("dKLen = {} , Count = {} , Rand = {} ".format(i,s3cure_rand0m.count,b64encode(rand).decode()))
+    return rand
+
+s3cure_rand0m.count = 0
+
+ecc = ECC.generate(curve='P-521',randfunc=s3cure_rand0m)
+private_key = ecc.export_key(format='PEM',passphrase=secure_passphrase,protection="PBKDF2WithHMAC-SHA1AndAES128-CBC")
+print(private_key)
+open('private.pem','w').write(private_key)
---
+++ Crypto/SecureShell/script.sh
@@ -0,0 +1,4 @@
+chmod 400 Crypto/SecureShell/private.pem
+
+ssh-keygen -f Crypto/SecureShell/private.pem -y > authorized_keys
+
