4661855f29d995c310e1197d6958b42040ca8007
HarshVaragiya <harshvaragiya8@gmail.com>
2020-02-25 12:57:43 +0530

    Added PunyFactors Challenge and added Faster JSCrypt Crack Codes

--- Crypto/JSCrypt/crack.py
+++ Crypto/JSCrypt/crack.py
@@ -1,12 +1,10 @@
 import hashlib
 
-iterations = 0xFFFFF
+iterations = 0xFFFFFFF
 init_password = "awesome_secure_password"
 
 passwd = init_password.encode()
 for i in range(iterations):
     passwd = hashlib.sha256(passwd).digest()
-    if i % 0xFF :
-        print(100*i/iterations,end='\r')
 
 print(passwd.hex())
