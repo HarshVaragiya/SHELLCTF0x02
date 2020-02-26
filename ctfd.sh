4a97eea7505f943ef035fac675f5561cf4525533
HarshVaragiya <harshvaragiya8@gmail.com>
2020-02-27 02:13:29 +0530

    Fixed CTFd Script

--- ctfd.sh
+++ ctfd.sh
@@ -5,10 +5,12 @@ do
 	ret=0
 	for i in {1..5}
 	do
-		curl http://shellpwn.ml -I | grep -i "200"
+		curl http://shellpwn.ml -I | grep -i "internal"
+		code=$?
+		echo "Exit Code : $code"
 		sleep 1
 		echo $ret
-		if [[ $? -ne 1 ]]
+		if [[ $code -ne 1 ]]
 		then
 			ret=$(($ret+1))
 		fi
@@ -21,6 +23,7 @@ do
 		fuser -k -n tcp 80
 		sleep 2	
 		echo "Restarting Session!"
-		tmux new-session -d -s ctfd '/usr/bin/python3 /home/ubuntu/ctf/0x02/serve.py --port 80'
-	fi
+		#tmux new-session -d -s ctfd '/usr/bin/python3 /home/ubuntu/ctf/0x02/serve.py --port 80'
+		echo "fuxserver"	
+fi
 done
