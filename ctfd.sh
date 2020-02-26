35f042a812520cbc51472b672098ebdb04fa8afc
HarshVaragiya <harshvaragiya8@gmail.com>
2020-02-27 01:59:49 +0530

    Fixed CTFd Script

--- ctfd.sh
+++ ctfd.sh
@@ -1,28 +1,26 @@
 #!/bin/bash
 
-
 while test "1" = "1"
 do
-ret=0
-for i in {1..5}
-do
+	ret=0
+	for i in {1..5}
+	do
+		curl http://shellpwn.ml -I | grep -i "200"
+		sleep 1
+		echo $ret
+		if [[ $? -ne 1 ]]
+		then
+			ret=$(($ret+1))
+		fi
+		echo $ret
+	done
 
-curl http://shellpwn.ml -I | grep -i "200"
-sleep 1
-if [[ $? -ne 1 ]]
-then
-ret=$(($ret+1))
-fi
-
+	if [[ $ret -eq 5 ]]
+	then
+		echo "Fucking Servers"
+		fuser -k -n tcp 80
+		sleep 2	
+		echo "Restarting Session!"
+		tmux new-session -d -s ctfd '/usr/bin/python3 /home/ubuntu/ctf/0x02/serve.py --port 80'
+	fi
 done
-
-if [[ $ret -eq 5 ]]
-then
-fuser -k -n tcp 80
-sleep 2
-tmux new -s ctfd -d '/usr/bin/python3 /home/ubuntu/ctf/0x02/serve.py --port 80'
-fi
-
-
-done
-
